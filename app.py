96e007a9a4c2d65f1b33b1a70c55e6a8551588a2
s140674-bit <s140674@student.squ.edu.om>
2025-12-10 19:33:38 +0400

    Update app.py

--- app.py
+++ app.py
@@ -301,7 +301,7 @@ if uploaded:
     Cf = float(init.get("Cf", 0))
     #calculate deltaP
     Pi = prod["p"].iloc[0]
-    prod["Efw"] = (Boi*(Cw * Swc) + Cf)(Pi - prod["p"]) / (1 - Swc) 
+    prod["Efw"] = Boi * ((Cw * Swc) + Cf) * (Pi - prod["p"]) / (1 - Swc)
     prod["Eo"] = (prod["Bo"] - Boi )+ ((Rsi - prod["Rs"]) * prod["Bg"])
     prod["Eg"] = Boi*((prod["Bg"]/Bgi)-1)
     # --- Compute Rp exactly like Excel --- 
