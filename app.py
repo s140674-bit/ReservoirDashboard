1ecd913dedf15befa555352a1ba474a1695d7b99
s140674-bit <s140674@student.squ.edu.om>
2025-12-10 20:39:18 +0400

    Update app.py

--- app.py
+++ app.py
@@ -322,8 +322,7 @@ if uploaded:
     
     # --- Linear Regression (Straight-Line Fit) ---
     if len(prod_clean) > 1:
-        # Remove first 2 unstable points
-        reg_data = prod_clean.sort_values("x", ascending=True).iloc[2:]
+        reg_data = prod_clean.sort_values("x", ascending=True).iloc[:6]
         # Perform linear regression on correct data
         coeffs = np.polyfit(reg_data["x"], reg_data["y"], 1)
         Nm = coeffs[0]
